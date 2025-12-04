762ad73580922df6a9abb4eb1badfd122fc21fdc
wh1t3zZnb <wh1t3zZnb@users.noreply.github.com>
2025-12-05 01:03:33 +0800

    fix: explicitly define process.env for build

--- vite.config.js
+++ vite.config.js
@@ -3,6 +3,9 @@ import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   base: '/',
+  define: {
+    'process.env': process.env
+  },
   plugins: [react()],
   server: { port: 5173, strictPort: true }
 })
